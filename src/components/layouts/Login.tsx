4b30d23e78e55f8fb9d9dba175e242d390c1f24c
Shallfeu <chigvincevra@mail.ru>
2022-09-02 17:03:18 +0300

    search completed

--- src/components/layouts/Login.tsx
+++ src/components/layouts/Login.tsx
@@ -55,6 +55,7 @@ const Login: React.FC = () => {
 
   useEffect(() => {
     validate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   const handleChange = ({ target }: any) => {
