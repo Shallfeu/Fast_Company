ed694bebea34f97f89cd60b5075a1fdcdd2e0a99
Shallfeu <chigvincevra@mail.ru>
2022-11-07 15:46:35 +0300

    fix date bug

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,41 +1,27 @@
-export function date(time: number) {
-  const datePost = new Date(time);
+export function date(data: number) {
+  const date = new Date(data);
   const dateNow = new Date();
-  const months: {} = {
-    "0": "January",
-    "1": "February",
-    "2": "March",
-    "3": "April",
-    "4": "May",
-    "5": "June",
-    "6": "July",
-    "7": "August",
-    "8": "September",
-    "9": "October",
-    "10": "November",
-    "11": "December",
-  };
-
-  const yearDif = dateNow.getFullYear() - datePost.getFullYear();
+  const yearDif = dateNow.getFullYear() - date.getFullYear();
   if (yearDif === 0) {
-    const dayDif = dateNow.getDay() - datePost.getDay();
+    const dayDif = dateNow.getDate() - date.getDate();
     if (dayDif === 0) {
-      const hoursDif = dateNow.getHours() - datePost.getHours();
-      if (hoursDif === 0) {
-        const minuteDif = dateNow.getMinutes() - datePost.getMinutes();
-        if (minuteDif < 1) return "1 минуту назад";
-        if (minuteDif < 5) return "5 минут назад";
-        if (minuteDif < 10) return "10 минут назад";
+      const hourDif = dateNow.getHours() - date.getHours();
+      if (hourDif === 0) {
+        const minutesDif = dateNow.getMinutes() - date.getMinutes();
+
+        if (minutesDif >= 0 && minutesDif < 5) return "1 минуту назад";
+        if (minutesDif >= 5 && minutesDif < 10) return "5 минут назад";
+        if (minutesDif >= 10 && minutesDif < 30) {
+          return "10 минут назад";
+        }
         return "30 минут назад";
       }
-      return `${datePost.getHours()}:${datePost.getMinutes()}`;
+      return `${date.getHours()}:${date.getMinutes()}`;
     }
-    return `${datePost.getDay()} ${
-      months[datePost.getMonth() as keyof typeof months]
-    }`;
-  }
 
-  return `${datePost.getDay()} ${
-    months[datePost.getMonth() as keyof typeof months]
-  } ${datePost.getFullYear()}`;
+    return `${date.getDate()} ${date.toLocaleString("default", {
+      month: "long",
+    })}`;
+  }
+  return `${date.getFullYear()}.${date.getMonth() + 1}_${date.getDate()}`;
 }
